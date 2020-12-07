edfdbbc4ceeefd67ea660aa85ad0bab1925e4905
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2020-12-06 21:32:07 -0500

    Update NB_BOW_OV2.py

--- NB_BOW_OV2.py
+++ NB_BOW_OV2.py
@@ -33,108 +33,13 @@ df = pd.DataFrame(doc_term_matrix,
                   columns= vectorizer.get_feature_names())
 
 
-# COMPUTE CONDITIONALS
-# Vocabulary = all words in the tweets of the training set
 
-# Step 1: Split the tweets based on yes or no
-#
-# numTweetsNo = 0
-# numTweetsYes = 0
-# yesTweets = []
-# noTweets = []
-# finalYesTweets = []
-# finalNoTweets = []
-#
-# for i in range(numrows):
-#     if arr[i][2] == "no":
-#         numTweetsNo += 1
-#         noTweets.append(arr[i][1])
-#     if arr[i][2] == "yes":
-#         numTweetsYes += 1
-#         yesTweets.append(arr[i][1])
-#
-# # Step 2 : Count each instances of every word from the vocabulary in YesTweets
-# sizeYes = len(yesTweets)
-#
-# for i in range(sizeYes):
-#     tokenizer = RegexpTokenizer(r'\w+')
-#     s = tokenizer.tokenize(yesTweets[i])
-#     finalYesTweets.append(s)
-# finalYesTweets = list(chain.from_iterable(finalYesTweets))
-#
-# # Step 3 : Count each instances of every word from the vocabulary in NoTweets
-#
-# sizeNo = len(noTweets)
-# for i in range(sizeNo):
-#     tokenizer = RegexpTokenizer(r'\w+')
-#     s = tokenizer.tokenize(noTweets[i])
-#     finalNoTweets.append(s)
-# finalNoTweets = list(chain.from_iterable(finalNoTweets))
-#
-# # Count number of words in each dictionary
-# totalWordsNo = len(finalNoTweets)
-# totalWordsYes = len(finalYesTweets)
-#
-# yesDictionary = dict()
-# noDictionary = dict()
-# # Initialize "yes" tweets dictionary with training set
-# for i in range(len(finalVocabulary)):
-#     val = finalYesTweets.count(finalVocabulary[i])
-#     yesDictionary[finalVocabulary[i]] = val
-#
-# # Initialize "No" tweets dictionary with training set
-# for i in range(len(finalVocabulary)):
-#     val = finalNoTweets.count(finalVocabulary[i])
-#     noDictionary[finalVocabulary[i]] = val
-#
-#
-# # COMPUTE PRIORS
-# # Calculate probabilities of each class (yes, no for factual tweet)
-# totalTweets = numTweetsNo + numTweetsYes
-# priorYes = numTweetsYes / totalTweets
-# priorNo = numTweetsNo / totalTweets
-#
 # # Apply the model on the test set
 tsv_file = open("covid_test_public.tsv")
 read_tsv = csv.reader(tsv_file, delimiter="\t")
 testArr = []
 for row in read_tsv:
     testArr.append(row)
-
-
-# testTweets = []
-# for i in range(len(testArr)):
-#     tokenizer = RegexpTokenizer(r'\w+')
-#     s = tokenizer.tokenize(testArr[i][1])
-#     testTweets.append(s)
-#
-#
-# def calculateCondYes(freq):
-#     prob = math.log10((freq + 0.01) / (totalWordsYes + len(finalVocabulary)))
-#     return prob
-#
-#
-# def calculateCondNo(freq):
-#     prob = math.log10((freq + 0.01) / (totalWordsNo + len(finalVocabulary)))
-#     return prob
-#
-#
-# scoreYes = 1  # can't be 0 to start
-# for item in testTweets[2]:
-#     if item in yesDictionary:
-#         scoreYes *= calculateCondYes(yesDictionary[item])
-#
-# scoreYes *= math.log10(priorYes)
-# print("Score Yes : " + str(scoreYes))
-#
-# # Total No conditionals
-# scoreNo = 1  # can't be 0 to start
-# for item in testTweets[2]:
-#     if item in noDictionary:
-#         scoreNo *= calculateCondNo(noDictionary[item])
-#
-# scoreNo *= math.log10(priorNo)
-# print("Score No : " + str(scoreNo))
 
 
 # Writing the Trace file
