ac46623d6bea04e555ad8e1c7a1b0f29f204733a
Charles-Antoine Guité <charlotguite@hotmail.fr>
2020-12-11 15:05:18 -0500

    Added new extraction method

--- NB_BOW_OV.py
+++ NB_BOW_OV.py
@@ -1,143 +1,76 @@
 import csv
 from itertools import chain
 import math
-from nltk.tokenize import RegexpTokenizer
+from collections import defaultdict
 
-tsv_file = open("covid_training.tsv", encoding="UTF-8")
-read_tsv = csv.reader(tsv_file, delimiter="\t")
-arr = []
+file1 = open("covid_training.tsv", 'r', encoding="utf-8")
+Tweets = file1.readlines()
 
-for row in read_tsv:
-    arr.append(row)
+vocabDictionary = defaultdict(int)
+filteredVocabDictionary = defaultdict(int)
 
-arr.remove(arr[0])
-numrows = len(arr)
-numcols = len(arr[0])
-
-# ClEANING THE DATA AND PREPARE IT FOR VOCABULARY
-
-# Fold training set in lowercase
-for i in range(numrows):
-    arr[i][1] = arr[i][1].lower()
-
-vocabularyArr = []
-vocabulary = []
-# # Build a list of all words in training set (bag of words)
-#
-#
-for i in range(numrows):
-    tokenizer = RegexpTokenizer(r'\w+')
-    s = tokenizer.tokenize(arr[i][1])
-    vocabularyArr.append(s)
-
-for i in range(numrows):
-    singleSize = len(vocabularyArr[i])
-    for y in range(singleSize):
-        vocabulary.append(vocabularyArr[i][y])
-
-# To remove duplicates from the vocabulary
-vocabulary = list(set(vocabulary))
-seen = set()
-finalVocabulary = []
-for item in vocabulary:
-    if item not in seen:
-        seen.add(item)
-        finalVocabulary.append(item)
-
-# COMPUTE CONDITIONALS
-# Vocabulary = all words in the tweets of the training set
-
-# Step 1: Split the tweets based on yes or no
-
-numTweetsNo = 0
-numTweetsYes = 0
 yesTweets = []
 noTweets = []
-finalYesTweets = []
-finalNoTweets = []
+yesDictionary = defaultdict(int)
+noDictionary = defaultdict(int)
 
-for i in range(numrows):
-    if arr[i][2] == "no":
-        numTweetsNo += 1
-        noTweets.append(arr[i][1])
-    if arr[i][2] == "yes":
-        numTweetsYes += 1
-        yesTweets.append(arr[i][1])
+for tweet in Tweets:
+    tweet = tweet.lower()
+    tempArray = tweet.split()
+    #for index, item in enumerate(tempArray):
+     #   if item[-1] in ('.', ',', '!', '?'):
+      #      tempArray[index] = item[:-1]
+    tempArray = tempArray[1:-6]
+    if tempArray[len(tempArray) - 1] == "yes":
+        tempArray.pop(len(tempArray) - 1)
+        yesTweets.append(" ".join(tempArray))
+    else:
+        tempArray.pop(len(tempArray) - 1)
+        noTweets.append(" ".join(tempArray))
+    for elem in tempArray:
+        vocabDictionary[elem] += 1
 
-# Step 2 : Count each instances of every word from the vocabulary in YesTweets
-sizeYes = len(yesTweets)
+for item in yesTweets:
+    for word in item.split():
+        yesDictionary[word] += 1
 
-for i in range(sizeYes):
-    tokenizer = RegexpTokenizer(r'\w+')
-    s = tokenizer.tokenize(yesTweets[i])
-    finalYesTweets.append(s)
-finalYesTweets = list(chain.from_iterable(finalYesTweets))
+for item in noTweets:
+    for word in item.split():
+        noDictionary[word] += 1
 
-# Step 3 : Count each instances of every word from the vocabulary in NoTweets
-
-sizeNo = len(noTweets)
-for i in range(sizeNo):
-    tokenizer = RegexpTokenizer(r'\w+')
-    s = tokenizer.tokenize(noTweets[i])
-    finalNoTweets.append(s)
-finalNoTweets = list(chain.from_iterable(finalNoTweets))
-
-# Count number of words in each dictionary
-totalWordsNo = len(finalNoTweets)
-totalWordsYes = len(finalYesTweets)
-
-yesDictionary = dict()
-noDictionary = dict()
-# Initialize "yes" tweets dictionary with training set
-for i in range(len(finalVocabulary)):
-    val = finalYesTweets.count(finalVocabulary[i])
-    yesDictionary[finalVocabulary[i]] = val
-
-# Initialize "No" tweets dictionary with training set
-for i in range(len(finalVocabulary)):
-    val = finalNoTweets.count(finalVocabulary[i])
-    noDictionary[finalVocabulary[i]] = val
-
-# total = 0
-# for item in yesDictionary:
-#     total += yesDictionary[item]
-# print(total)
-#
-# total1 = 0
-# for item in noDictionary:
-#     total1 += noDictionary[item]
-# print(total1)
+#filteredVocabDictionary = {key: val for key, val in vocabDictionary.items() if val != 1}
+#filteredYesDictionary = {key: val for key, val in yesDictionary.items() if val != 1}
+#filteredNoDictionary = {key: val for key, val in noDictionary.items() if val != 1}
 
 # COMPUTE PRIORS
 # Calculate probabilities of each class (yes, no for factual tweet)
-totalTweets = numTweetsNo + numTweetsYes
-priorYes = numTweetsYes / totalTweets
-priorNo = numTweetsNo / totalTweets
+totalTweets = len(yesTweets) + len(noTweets)
+priorYes = len(yesTweets) / totalTweets
+priorNo = len(noTweets) / totalTweets
 
 # Apply the model on the test set
 
-tsv_file = open("covid_test_public.tsv")
-read_tsv = csv.reader(tsv_file, delimiter="\t")
-testArr = []
-for row in read_tsv:
-    testArr.append(row)
-
+tsv_file = open("covid_test_public.tsv", 'r', encoding="utf-8")
+Tweets2 = tsv_file.readlines()
 testTweets = []
-for i in range(len(testArr)):
-    tokenizer = RegexpTokenizer(r'\w+')
-    s = tokenizer.tokenize(testArr[i][1])
-    testTweets.append(s)
 
+for tweet in Tweets2:
+    tweet = tweet.lower()
+    tempArray = tweet.split()
+    #for index, item in enumerate(tempArray):
+      # if item[-1] in ('.', ',', '!', '?'):
+      #      tempArray[index] = item[:-1]
+    tempArray = tempArray[:-6]
+    testTweets.append(" ".join(tempArray))
 
 def calculateCondYes(freq):
-    prob = math.log10((freq + 0.01) / (totalWordsYes + (len(finalVocabulary) * 0.01)))
+    prob = math.log10((freq + 0.01) / (sum(yesDictionary.values()) + (len(vocabDictionary) * 0.01)))
     return prob
 
 
 def calculateCondNo(freq):
-    prob = math.log10((freq + 0.01) / (totalWordsNo + (len(finalVocabulary) * 0.01)))
+    prob = math.log10((freq + 0.01) / (sum(noDictionary.values()) + (len(vocabDictionary) * 0.01)))
     return prob
-
 
 numCorrect = 0
 numWrong = 0
@@ -170,12 +103,14 @@ for i in range(len(testTweets)):
         modelPrediction = "yes"
         finalScore = scoreYes
 
-    if testArr[i][2] == "no" and modelPrediction == "yes":
+    first, *middle, last = testTweets[i].split()
+
+    if last == "no" and modelPrediction == "yes":
         conclusion = "wrong"
         fpYes += 1
         fnNo += 1
         numWrong += 1
-    elif testArr[i][2] == "yes" and modelPrediction == "no":
+    elif last == "yes" and modelPrediction == "no":
         conclusion = "wrong"
         fpNo += 1
         fnYes += 1
@@ -184,9 +119,9 @@ for i in range(len(testTweets)):
         conclusion = "correct"
         numCorrect += 1
 
-    f.write(str(testArr[i][0]) + "  " + str(modelPrediction) + "  " + str("{:e}".format(finalScore)) + "  " + str(
-        testArr[i][2]) + "  " +
-            str(conclusion) + "\n")
+   # f.write(str(testArr[i][0]) + "  " + str(modelPrediction) + "  " + str("{:e}".format(finalScore)) + "  " + str(
+    #    testArr[i][2]) + "  " +
+    #        str(conclusion) + "\n")
 
 
 def calculateAccuracy():
@@ -195,8 +130,8 @@ def calculateAccuracy():
 
 
 print("Accuracy : " + str(calculateAccuracy()))
-
-
+print(numCorrect)
+print(numWrong)
 #
 # TP = nb of instances that are in class C and that the model identified as C
 # FP = nb of instances that the model labelled as class C
@@ -225,11 +160,9 @@ def calculateF1No():
     return (2 * calculatePrecisionNo() * calculateRecallNo()) / (calculatePrecisionNo() + calculateRecallNo())
 
 
+#f = open("eval_NB-BOW-OV.txt", "w+", encoding="UTF-8")
+#f.write(str(calculateAccuracy()) + "\n" + str(calculatePrecisionYes())
+#        + "  " + str(calculatePrecisionNo()) + "\n" + str(calculateRecallYes())
+ #       + "  " + str(calculateRecallNo()) + "\n" + str(calculateF1Yes()) + "  " + str(calculateF1No()) + "\n")
 
-f = open("eval_NB-BOW-OV.txt", "w+", encoding="UTF-8")
-f.write(str(calculateAccuracy()) + "\n" + str(calculatePrecisionYes())
-        + "  " + str(calculatePrecisionNo()) + "\n" + str(calculateRecallYes())
-        + "  " + str(calculateRecallNo()) + "\n" + str(calculateF1Yes()) + "  " + str(calculateF1No()) + "\n")
-
-
-f.close()
+#f.close()
